d48869e8a08e6fc90dd46d6b26ff0a09da0ebc12
Yichen Huang <yichen0831@gmail.com>
2016-01-08 13:34:34 +0800

    Added BlockImagePattern and removed draw_block for performance

--- game/game_window.py
+++ game/game_window.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+import random
 import pyglet
 from pyglet.gl import *
+from pyglet.image import ImagePattern, ImageData
 from pyglet.window import Window
 
 pyglet.resource.path = ['resources']
@@ -7,14 +10,16 @@ pyglet.resource.reindex()
 pyglet.resource.add_font('MONOFONT.TTF')
 
 
-WHITE = (0.9, 0.9, 0.9, 1.0)
-BLACK = (0.2, 0.2, 0.2, 1.0)
-RED = (1.0, 0.5, 0.5, 1.0)
-GREEN = (0.5, 1.0, 0.5, 1.0)
-BLUE = (0.5, 0.5, 1.0, 1.0)
-YELLOW = (0.8, 0.8, 0.5, 1.0)
-GREY = (0.7, 0.7, 0.7, 1.0)
-DARK_GREY = (0.5, 0.5, 0.5, 1.0)
+BLACK = (0.2, 0.2, 0.2, 1.0)      # color 0
+RED = (1.0, 0.5, 0.5, 1.0)        # color 1
+GREEN = (0.5, 1.0, 0.5, 1.0)      # color 2
+BLUE = (0.5, 0.5, 1.0, 1.0)       # color 3
+YELLOW = (0.8, 0.8, 0.5, 1.0)     # color 4
+GREY = (0.7, 0.7, 0.7, 1.0)       # color 5
+DARK_GREY = (0.5, 0.5, 0.5, 1.0)  # color 6
+WHITE = (0.9, 0.9, 0.9, 1.0)      # color 7
+
+COLORS = [BLACK, RED, GREEN, BLUE, YELLOW, GREY, DARK_GREY, WHITE]
 
 
 def int_color(float_color):
@@ -22,11 +27,35 @@ def int_color(float_color):
 
 
 def lighten_color(float_color):
-    return tuple(map(lambda c: c*1.2, float_color))
+    return tuple(map(lambda c: min(c*1.2, 1.0), float_color))
 
 
 def darken_color(float_color):
     return tuple(map(lambda c: c*0.8, float_color))
+
+
+class BlockImagePattern(ImagePattern):
+    def __init__(self, color):
+        self.color = int_color(color)
+        self.light_color = int_color(lighten_color(color))
+        self.dark_color = int_color(darken_color(color))
+
+    def create_image(self, width, height):
+        data = b''
+        for i in range(width * height):
+            pos_x = i % width
+            pos_y = i // width
+            if pos_x / width <= 0.08:
+                data += bytes(self.light_color)
+            elif pos_y / height <= 0.08:
+                data += bytes(self.dark_color)
+            elif pos_y / height >= 0.92:
+                data += bytes(self.light_color)
+            elif pos_x / width >= 0.92:
+                data += bytes(self.dark_color)
+            else:
+                data += bytes(self.color)
+        return ImageData(width, height, 'RGBA', data)
 
 
 class GameWindow(Window):
@@ -50,6 +79,13 @@ class GameWindow(Window):
         self.bottom = self.height / 2 - self.block_size * 11
         self.top = self.height / 2 + self.block_size * 11
 
+        self.BLOCK_IMAGES = [
+            pyglet.image.create(int(self.block_size), int(self.block_size),
+                                BlockImagePattern(color)) for color in COLORS
+        ]
+
+        self.board = [0 for _ in range(10 * 20)]
+
         font = pyglet.font.load(name='Monofonto', size=16)
         self.clock_display = pyglet.clock.ClockDisplay(font=font,
                                                        color=(.8, .8, .8, .8))
@@ -68,52 +104,24 @@ class GameWindow(Window):
 
     def on_draw(self):
         self.clear()
-        self.draw_block(1, 1, color=RED)
+        self.draw_board()
         self.batch.draw()
         self.clock_display.draw()
 
     def update(self, delta):
         pass
 
-    def draw_block(self, pos_x, pos_y, color=WHITE):
+    def draw_board(self):
         size = self.block_size
-        x = self.left + (pos_x + 1) * size
-        y = self.bottom + (pos_y + 1) * size
-        colors = color * 4
-        dark_colors = darken_color(color) * 4
-        light_colors = lighten_color(color) * 4
 
-        pyglet.graphics.draw(4, GL_QUADS,
-                             ('v2f', (x, y,
-                                      x+size, y,
-                                      x+size, y+size,
-                                      x, y+size)),
-                             ('c4f', colors))
-        pyglet.graphics.draw(4, GL_QUADS,
-                             ('v2f', (x, y,
-                                      x+size, y,
-                                      x+size, y+size*0.1,
-                                      x, y+size*0.1)),
-                             ('c4f', dark_colors))
-        pyglet.graphics.draw(4, GL_QUADS,
-                             ('v2f', (x+size*0.9, y,
-                                      x+size, y,
-                                      x+size, y+size,
-                                      x+size*0.9, y+size)),
-                             ('c4f', dark_colors))
-
-        pyglet.graphics.draw(4, GL_QUADS,
-                             ('v2f', (x, y,
-                                      x+size*0.1, y,
-                                      x+size*0.1, y+size,
-                                      x, y+size)),
-                             ('c4f', light_colors))
-        pyglet.graphics.draw(4, GL_QUADS,
-                             ('v2f', (x, y+size*0.9,
-                                      x+size*0.9, y+size*0.9,
-                                      x+size*0.9, y+size,
-                                      x, y+size)),
-                             ('c4f', light_colors))
+        for i, v in enumerate(self.board):
+            if v == 0:
+                continue
+            pos_x = i % 10
+            pos_y = i // 10
+            x = self.left + (pos_x + 1) * size
+            y = self.bottom + (pos_y + 1) * size
+            self.BLOCK_IMAGES[v].blit(x, y)
 
     def make_background(self):
         color = DARK_GREY * 4
